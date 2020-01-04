aea044a1c13612cbeabf5ce2d2ef2452502cc490
João Gabriel <desenvolvedorjg@gmail.com>
2020-01-04 16:20:12 -0300

    [ADD] Implementação inicial do componente app e suas rotas.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import logo from './logo.svg';
+// import './App.css';
+
+import './global.css';
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Cliente from "./components/Cliente";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/cliente" component={Cliente}/>
+          {/*<Route exact path="/register" component={Register}/>*/}
+          {/*<Route exact path="/cliente/new" component={New}/>*/}
+        </Switch>
+      </BrowserRouter>
   );
 }
 
