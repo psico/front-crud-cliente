2bac8eb2139e0f844ab71105a697694c28e0df70
João Gabriel <desenvolvedorjg@gmail.com>
2020-01-04 21:01:59 -0300

    [ADD] Adicionado Axios ao projeto.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,33 +1,7 @@
 import React, {Component} from 'react';
-// import firebase from "../../firebase";
 import './home.css';
 
 class Home extends Component {
-
-    state = {
-        posts: []
-    }
-
-    componentDidMount() {
-        // firebase.app.ref('posts').on('value', (snapshot) => {
-        //     let state = this.state;
-        //     state.posts = [];
-        //     snapshot.forEach((childItem) => {
-        //         state.posts.push({
-        //             key: childItem.key,
-        //             titulo: childItem.val().titulo,
-        //             image: childItem.val().image,
-        //             descricao: childItem.val().descricao,
-        //             autor: childItem.val().autor,
-        //         })
-        //     })
-        //
-        //     state.posts.reverse();
-        //     this.setState(state);
-        // })
-
-    }
-
     render() {
         return (
             <div>
@@ -36,13 +10,15 @@ class Home extends Component {
                 <p>
                     - Banco de dados: PostGreSQL e Power Architect (Modelagem).<br/>
                     - Backend: Java, Springboot, JPA, Liquibases.<br/>
-                    - Frontend: Javascript, React.
+                    - Frontend: Javascript, React, Axios.
                 </p>
 
                 <p>
-                    Explicando algumas escolhas:<br />
-                    - Por que não react-native? Para ganhar tempo no desenvolvimento e não ter que se preocupar com maquinas virtuais e compatibilidade e também por que não foi pedido.<br />
-                    - Por que o BD PostgreSQL? É um dos mais simples e rapidos de configurar sem surpresas, seja na instalação seja para configurar o backend.
+                    Explicando algumas escolhas:<br/>
+                    - Por que não react-native? Para ganhar tempo no desenvolvimento e não ter que se preocupar com
+                    maquinas virtuais e compatibilidade e também por que não foi pedido.<br/>
+                    - Por que o BD PostgreSQL? É um dos mais simples e rapidos de configurar sem surpresas, seja na
+                    instalação seja para configurar o backend.
                 </p>
             </div>
         );
